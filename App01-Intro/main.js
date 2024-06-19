57c92c5a031300d2cb3bab97f02d37216f003f1e
IgnaDev <arcortoon@gmail.com>
2024-06-19 17:52:01 -0300

    Agregado lo necesario para que se cierre en mac

--- App01-Intro/main.js
+++ App01-Intro/main.js
@@ -14,3 +14,24 @@ function crearVentanaPrincipal(){
 }
 
 app.whenReady().then(crearVentanaPrincipal)
+
+// Esta función se ejecuta cuando todas las ventanas de la aplicación están cerradas.
+app.on('window-all-closed', function(){
+    // Si el sistema operativo es macOS (darwin), entonces cerramos la aplicación.
+    // En macOS, es común mantener la aplicación activa incluso cuando todas las ventanas están cerradas,
+    // por eso se comprueba si el sistema operativo es 'darwin' antes de llamar a app.quit().
+    if(process.platform === 'darwin'){
+        app.quit()
+    }
+})
+
+// Esta función se ejecuta cuando la aplicación se activa, por ejemplo, cuando el usuario hace clic en el icono
+// de la aplicación en el Dock en macOS.
+app.on('activate', function(){
+    // Si no hay ventanas abiertas en la aplicación, creamos una nueva ventana principal.
+    // Esto es importante en macOS, donde es común que la aplicación permanezca abierta sin ventanas visibles,
+    // y el usuario puede esperar que una ventana se abra cuando se reactiva la aplicación.
+    if(BrowserWindow.getAllWindows().length === 0){
+        crearVentanaPrincipal()
+    }
+})
